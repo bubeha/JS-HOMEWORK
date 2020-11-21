51eeaa59e54a736215e4a5ec16517b276b811167
cubbi3334 <cubbije@gmail.com>
2020-11-21 12:56:02 +0300

    first-commite

---
+++ homework-1/script.js
@@ -0,0 +1,43 @@
+// Исправьте синтаксические ошибки
+
+function addNumbers(a, b, c) {
+    return a + b + c;
+}
+
+function introduceMe(name, age) {
+    return "Привет, меня зовут " + name + "и мне" + age + "года";
+}
+
+function getTotal(a, b) {
+    total = a + b;
+    return "The total is total";
+}
+
+// Допишите функцию так, чтобы теперь она принимала параметры
+function multiply(a, b) {
+    // посчитайте результат и верните его
+    return a * b
+}
+
+// функция должна возвращать произведение двух чисел
+var result = multiply(3, 4);
+
+console.log(result);
+
+// Напишите функцию divide которая получает частное двух переданных ей чисел
+function divide(a, b) {
+    return a / b
+}
+var result = divide(3, 4);
+
+console.log(result);
+
+
+// Напишите функцию sum которая складывает два числа
+function plus(a, b) {
+    return a + b
+}
+// Вызовите функцию и запишите ее возвращаемое значение в переменную sum
+var sum = plus(1, 10);
+console.log(sum);
+
